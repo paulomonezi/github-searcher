5104308a5027de21db83d9f648da8d3118b80537
Paulo Monezi <paulo.monezi@icloud.com>
2022-06-29 22:06:00 -0400

    refactoring

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import { user } from '/src/scripts/services/user.js'
+import { repositories } from '/src/scripts/services/repositories.js'
+
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
     getUserProfile(userName)
@@ -13,21 +16,8 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     }
 })
 
-async function user(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
-    return await response.json()
-}
-
-async function repos(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
-    return await response.json()
-}
-
 function getUserProfile(userName) {
 
-    repos(userName).then(reposData => {
-        console.log(reposData)
-    })
 
     user(userName).then((userData) => {
         let userInfo = `<div class="info">
@@ -44,7 +34,7 @@ function getUserProfile(userName) {
 }
 
 function getUserRepositories(userName) {
-    repos(userName).then(reposData => {
+    repositories(userName).then(reposData => {
         let repositoriesItems = ""
         reposData.forEach(repo => {
             repositoriesItems += `<li><a href="${repo.html_url}" target=_blank>${repo.name}</li>`
@@ -56,5 +46,3 @@ function getUserRepositories(userName) {
                                                                 </div>`
     })
 }
-
-
